9d3351e29bc8ede692c58fc563c0b6c8faabeb4e
merdie <merdiemlb@gmail.com>
2021-04-07 15:02:43 +0100

    mise à jour des modifications apportées

--- app.js
+++ app.js
@@ -46,12 +46,16 @@ window.setInterval(()=> {
 }, 200);
 
 // récupération des éléments dans la base des données
+
 window.onload = function () {
     const homes__name = document.querySelector("#homes__name");
     const aboutMe__identity__name = document.querySelector(".aboutMe__identity__name");
     const aboutMe__identity__profile = document.querySelector(".aboutMe__identity__profile");
     const aboutMe__identity__email = document.querySelector(".aboutMe__identity__email");
     const aboutMe__identity__phone = document.querySelector(".aboutMe__identity__phone");
+    const aboutMe__me__biography = document.querySelector("#aboutMe__me__biography");
+
+    // dans le premier élément de la ressource. identité
 
     fetch("http://localhost:3000/identite").then(function(response){
         return response.json();
@@ -63,24 +67,85 @@ window.onload = function () {
             let profile = document.createTextNode(`${i.profile}`);
             let email = document.createTextNode(`${i.email}`);
             let phone = document.createTextNode(`${i.telephone}`);
+            let biography = document.createTextNode(`${i.biographie}`);
+           
             homes__name.appendChild(names);
             aboutMe__identity__name.appendChild(name);
             aboutMe__identity__profile.appendChild(profile);
             aboutMe__identity__email.appendChild(email);
             aboutMe__identity__phone.appendChild(phone);
+            aboutMe__me__biography.appendChild(biography);
         }
     })
+
+    const aboutMe__skills_1 = document.querySelector('#aboutMe__skills_1');
+    const aboutMe__skills_2 = document.querySelector('#aboutMe__skills_2');
+    const aboutMe__skills_3 = document.querySelector('#aboutMe__skills_3');
+    const aboutMe__skills_4 = document.querySelector('#aboutMe__skills_4');
+
+    // dans le deuxième élément compétence
+    fetch("http://localhost:3000/competences").then(function(response){
+        return response.json();
+    })
+    .then(function(data) {
+        for (let i of data) {
+            let skills1 = document.createTextNode(`${i.html} ${i.pourcentageHtml}`);
+            let skills2 = document.createTextNode(`${i.css} ${i.pourcentageCss}`);
+            let skills3 = document.createTextNode(`${i.javascript} ${i.pourcentageJavascript}`);
+            let skills4 = document.createTextNode(`${i.python} ${i.pourcentagePython}`);
+
+            aboutMe__skills_1.appendChild(skills1);
+            aboutMe__skills_2.appendChild(skills2);
+            aboutMe__skills_3.appendChild(skills3);
+            aboutMe__skills_4.appendChild(skills4);
+        }
+    })
+
+    // const projects__1_image = document.querySelector('#projects__1_image img');
+    // const projects__1_title = document.querySelector('#projects__1_title');
+    // const projects__1_description = document.querySelector('#projects__1_description');
+
+    // const projects__2_image = document.querySelector('#projects__2_image img');
+    // const projects__2_title = document.querySelector('#projects__2_title');
+    // const projects__2_description = document.querySelector('#projects__2_description');
+
+    // const projects__3_image = document.querySelector('#projects__3_image img');
+    // const projects__3_title = document.querySelector('#projects__3_title');
+    // const projects__3_description = document.querySelector('#projects__3_description');
+
+    // fetch("http://localhost:3000/Projets").then(function(response){
+    //     return response.json();
+    // })
+    // .then(function(data) {
+    //     for (let i = 0; i<= data.length; i++) {
+    //         if (i=0) {
+    //             let projects1__image = document.createTextNode(`${data[i].image}`);
+    //             let projects1__title = document.createTextNode(` ${data[i].titreProjet}`);
+    //             let projects1__description = document.createTextNode(`${data[i].descriptionProjet}`);
+
+    //             projects__1_image.appendChild(projects1__image);
+    //             projects__1_title.appendChild(projects1__title);
+    //             projects__1_description.appendChild(projects1__description);
+    //         }
+    //         if (i=1) {
+    //             let projects2__image = document.createTextNode(`${data[i].image}`)
+    //             let projects2__titre = document.createTextNode(` ${data[i].titreProjet}`)
+    //             let projects2__description = document.createTextNode(`${data[i].descriptionProjet}`)
+
+    //             projects__2_image.appendChild(projects__1_image);
+    //             projects__2_title.appendChild(projects__1_title);
+    //             projects__2_description.appendChild(projects__1_title);
+    //         }
+    //         if (i=2) {
+    //             let projects3__image = document.createTextNode(`${data[i].image}`)
+    //             let projects3__titre = document.createTextNode(` ${data[i].titreProjet}`)
+    //             let projects3__description = document.createTextNode(`${data[i].descriptionProjet}`)
+
+    //             projects__3_image.appendChild(projects__1_image);
+    //             projects__3_title.appendChild(projects__1_title);
+    //             projects__3_description.appendChild(projects__1_title);
+    //         }
+    //     }
+    // })
 }
 
-// window.onload = function () {
-//     const icone_contacts__logo = document.querySelector(".icone_contacts__logo");
-//     fetch("http://localhost:3000/icone_contacts")
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         for (let i of data) {
-//           let content = document.createTextNode(`${i.icones_contacts__logo}`);
-//           icone_contacts__logo.appendChild(content);
-//         }
-//       });
